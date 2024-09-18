d57dd127fe4e529f3f484735e711a01f504c01de
y-jiu <chldudwn99@gmail.com>
2024-09-19 01:44:51 +0900

    Fix mockdata

--- apps/provider/data-generator.py
+++ apps/provider/data-generator.py
@@ -55,40 +55,66 @@ def generate_two_months_data():
     }
     start_time = datetime.utcnow().replace(hour=0, minute=0, second=0, microsecond=0)
     data = []
-    total_production_time = 24 * 60  # 24시간 (초 단위)
+    total_production_time = 18 * 60  # 12시간 (분 단위)
     items_produced = 0
     production_time = 0
     tool_life_counter = 100
 
     while production_time < total_production_time:
         # 생산 시간 (G00 상태)
-        production_duration = random.randint(90, 110)  # 1분 30초 ~ 1분 50초
+        production_duration = random.randint(1, 2)  # 1~2분
         for _ in range(production_duration):
             current_time = start_time + timedelta(minutes=production_time)
             data_template["gcode"] = "G00"
             data_template["gcodegroupno"] = random.randint(0, 5)
             data_template["timestamp"] = current_time.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+            tool_life_counter -= random.uniform(0.6, 0.8)
             data_template["toollifecounter"] = tool_life_counter
-            tool_life_counter -= random.uniform(0.9, 1.1)
             data.append(data_template.copy())
             production_time += 1
 
         # 정지 시간 (G01 상태)
-        stop_duration = random.randint(10, 15)  # 10~15초 정지
-        for _ in range(stop_duration):
-            current_time = start_time + timedelta(minutes=production_time)
-            data_template["gcode"] = "G01"
-            data_template["gcodegroupno"] = random.randint(0, 5)
-            data_template["timestamp"] = current_time.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
-            data_template["toollifecounter"] = tool_life_counter
-            data.append(data_template.copy())
-            production_time += 1
+        stop_duration = 1  # 1분 정지
+        current_time = start_time + timedelta(minutes=production_time)
+        data_template["gcode"] = "G01"
+        data_template["gcodegroupno"] = random.randint(0, 5)
+        data_template["timestamp"] = current_time.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+        data_template["toollifecounter"] = tool_life_counter
+        data.append(data_template.copy())
+        production_time += 1
 
         items_produced += 1
+        print(items_produced)
+
+        if items_produced == 100:
+            rest_time = random.randint(90, 120)  # 60~90분 휴식
+            for _ in range(rest_time):
+                current_time = start_time + timedelta(minutes=production_time)
+                data_template["gcode"] = "G01"
+                data_template["gcodegroupno"] = random.randint(0, 5)
+                data_template["timestamp"] = current_time.strftime(
+                    "%Y-%m-%dT%H:%M:%S.%fZ"
+                )
+                data_template["toollifecounter"] = tool_life_counter
+                data.append(data_template.copy())
+                production_time += 1
+
+        if items_produced == 300:
+            rest_time = random.randint(60, 90)  # 30~40분 휴식
+            for _ in range(rest_time):
+                current_time = start_time + timedelta(minutes=production_time)
+                data_template["gcode"] = "G01"
+                data_template["gcodegroupno"] = random.randint(0, 5)
+                data_template["timestamp"] = current_time.strftime(
+                    "%Y-%m-%dT%H:%M:%S.%fZ"
+                )
+                data_template["toollifecounter"] = tool_life_counter
+                data.append(data_template.copy())
+                production_time += 1
 
         # 100개 생산 후 툴 교체
         if items_produced % 100 == 0:
-            tool_change_time = random.randint(300, 600)  # 5~10분 동안 툴 교체
+            tool_change_time = random.randint(5, 10)  # 5~10분 동안 툴 교체
             for _ in range(tool_change_time):
                 current_time = start_time + timedelta(minutes=production_time)
                 data_template["gcode"] = "G01"  # 툴 교체 중에는 G01 상태 유지
@@ -99,7 +125,8 @@ def generate_two_months_data():
                 data_template["toollifecounter"] = tool_life_counter
                 data.append(data_template.copy())
                 production_time += 1
-        tool_life_counter = 100
+
+            tool_life_counter = 100
 
     # 생산 종료 후 남은 시간을 G01 상태로 채우기
     while production_time < total_production_time:
@@ -110,10 +137,9 @@ def generate_two_months_data():
         data_template["toollifecounter"] = tool_life_counter
         data.append(data_template.copy())
         production_time += 1
-        tool_life_counter = 190
 
     # 데이터를 JSON 파일로 저장
-    with open("one_day_data4.json", "w") as f:
+    with open("18_hours_data.json", "w") as f:
         json.dump(data, f, indent=4)
 
 
