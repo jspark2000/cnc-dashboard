9e5bc7619c4cb5f515f1746ac9a324a16cacced2
jspark2000 <idpjs2000@gmail.com>
2024-12-13 16:05:06 +0900

    fix: mornitoring page tab

--- apps/frontend/src/pages/monitoring/SubNavigation.tsx
+++ apps/frontend/src/pages/monitoring/SubNavigation.tsx
@@ -1,14 +1,13 @@
 import queryString from 'query-string'
 import { useLocation, useNavigate } from 'react-router-dom'
+import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs'
+import { useEffect, useState } from 'react'
 
 const SubNavigation: React.FC = () => {
   const location = useLocation()
   const navigate = useNavigate()
   const query = queryString.parse(location.search)
-
-  const classNames = (...classes: string[]) => {
-    return classes.filter(Boolean).join(' ')
-  }
+  const [tab, setTab] = useState<string | undefined>()
 
   const changeQueryString = (newParams: any) => {
     const newQuery = { ...query, ...newParams }
@@ -16,48 +15,27 @@ const SubNavigation: React.FC = () => {
     navigate(`${location.pathname}?${newQueryString}`)
   }
 
+  useEffect(() => {
+    changeQueryString({ view: tab })
+  }, [tab])
+
+  useEffect(() => {
+    const query = queryString.parse(location.search)
+
+    setTab((query?.view as string) ?? 'cnc')
+  }, [])
+
   return (
     <div className="py-2 pl-4">
-      <div className="flex flex-row space-x-2 font-semibold">
-        <button
-          className={classNames(
-            query.view === 'cnc'
-              ? 'rounded-md border border-zinc-400 bg-zinc-800 px-4 py-2 text-zinc-50 shadow-sm'
-              : 'rounded-md border border-zinc-400 px-4 py-2 text-zinc-800 shadow-sm'
-          )}
-          onClick={() => {
-            changeQueryString({ view: 'cnc' })
-          }}
-          disabled={query.view === 'cnc'}
-        >
-          CNC
-        </button>
-        <button
-          className={classNames(
-            query.view === 'fft-stft'
-              ? 'rounded-md border border-zinc-400 bg-zinc-800 px-4 py-2 text-zinc-50 shadow-sm'
-              : 'rounded-md border border-zinc-400 px-4 py-2 text-zinc-800 shadow-sm'
-          )}
-          onClick={() => {
-            changeQueryString({ view: 'fft-stft' })
-          }}
-          disabled={query.view === 'fft-stft'}
-        >
-          FFT / STFT
-        </button>
-        <button
-          className={classNames(
-            query.view === 'features'
-              ? 'rounded-md border border-zinc-400 bg-zinc-800 px-4 py-2 text-zinc-50 shadow-sm'
-              : 'rounded-md border border-zinc-400 px-4 py-2 text-zinc-800 shadow-sm'
-          )}
-          onClick={() => {
-            changeQueryString({ view: 'features' })
-          }}
-          disabled={query.view === 'features'}
-        >
-          진동 및 전류 Features
-        </button>
+      <div className="flex basis-1/12 items-center space-x-8">
+        <h1 className="text-2xl font-bold text-zinc-800">실시간 모니터링</h1>
+        <Tabs value={tab} onValueChange={(value) => setTab(value)}>
+          <TabsList>
+            <TabsTrigger value="cnc">CNC 컨트롤러</TabsTrigger>
+            <TabsTrigger value="fft-stft">FFT/STFT</TabsTrigger>
+            <TabsTrigger value="features">Features</TabsTrigger>
+          </TabsList>
+        </Tabs>
       </div>
     </div>
   )
