32247579406f96aabf80b39f167bae4acdf91d06
jspark2000 <idpjs2000@gmail.com>
2024-12-17 17:26:20 +0900

    fix: cors setting

--- apps/preprocessing/app/utils/config.py
+++ apps/preprocessing/app/utils/config.py
@@ -2,7 +2,11 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.routes import router
 
-origins = ["http://localhost:7101", "http://localhost:5173"]
+origins = [
+    "http://localhost:7101",
+    "http://localhost:5173",
+    "http://192.168.30.211:5174",
+]
 
 
 def add_cors_middleware(app: FastAPI):
